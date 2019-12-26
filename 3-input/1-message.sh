dbca8f2821a0587d86a6b9391417c554367333d3
Matt <mjkern@andrew.cmu.edu>
2019-12-25 19:01:03 -0500

    cleaned up exercise 1 a little

--- 3-input/1-message.sh
+++ 3-input/1-message.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "please enter your first and last name:" FNAME LNAME
-read -p "and your height:" HEIGHT
+read -p "please enter your first and last name: " FNAME LNAME
+read -p "and your height: " HEIGHT
 
-echo "hi $LNAME, $FNAME! glad to hear that your are $HEIGHT"
+echo "hi $LNAME, $FNAME! glad to hear that your are $HEIGHT tall"
