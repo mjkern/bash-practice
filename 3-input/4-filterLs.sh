9b9608e67e20da718bc928a8653faa1b315174e3
Matt <mjkern@andrew.cmu.edu>
2019-12-25 22:33:59 -0500

    cleaned up 3.4

--- 3-input/4-filterLs.sh
+++ 3-input/4-filterLs.sh
@@ -3,4 +3,5 @@
 echo hopefully you piped some \'ls -l\' stuff into here...
 echo your filtered list:
 
-cat /dev/stdin | awk '{print $9 " (" $3 ")"}'
+# cut skips the first line, which is always "total [#]"
+cat /dev/stdin | cut -d $'\n' -f 2- | awk '{print $9 " (" $3 ")"}'
