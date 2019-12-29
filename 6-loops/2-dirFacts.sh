2dd387ea471c6a90b3bb826244420978ca2ad681
Matt <mjkern@andrew.cmu.edu>
2019-12-29 16:50:46 -0500

    exercise 2

--- 6-loops/2-dirFacts.sh
+++ 6-loops/2-dirFacts.sh
@@ -15,7 +15,7 @@ do
   path="$1$thing"
   if [ -d $path ]
   then
-    echo $thing is a directory
+    echo $thing is a directory that contains $(ls $path | wc -l) items
   elif [ -e $path ]
   then
     echo $thing is a $(stat -c %s $path)-byte file
