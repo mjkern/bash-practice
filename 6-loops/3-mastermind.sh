c460c642f6c892eca1f698c7ab2ea1740cb740a3
Matt <mjkern@andrew.cmu.edu>
2019-12-29 17:08:23 -0500

    generates the secret code

--- 6-loops/3-mastermind.sh
+++ 6-loops/3-mastermind.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-echo Welcome to mastermind! I already have thought of a secret code.
-echo It may contain any of the letters A, B, C, or D, possibly more
-echo than once. The code is four letters long. You may begin
-echo guessing by entering four \(space-separated\) letters.
+### SETUP ###
 
+# generate code
+CODE=''
+for I in {1..4}
+do
+  CODE="$CODE$(( $RANDOM % 4 + 1 )) "
+done
+echo the secret code is $CODE
+echo
+
+# notify user
+echo Welcome to mastermind! I already have thought of a secret code.
+echo It may contain any of the numbers 1, 2, 3, and 4, possibly more
+echo than once. The code is four numbers long. You may begin
+echo guessing by entering four \(space-separated\) numbers.
+echo
+
+### PLAY GAME ###
 read -p "enter a guess: " GUESS
