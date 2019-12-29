b265732f744d0a839288ff73fb3a061a10696795
Matt <mjkern@andrew.cmu.edu>
2019-12-29 12:01:31 -0500

    beginning exercise 1

---
+++ 4-arithmetic/1-mult.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo multiplying $1 and $2 with different methods:
+echo
+echo let:
+let A="$1*$2"
+echo $A
+echo
+echo expr:
+expr $1 \* $2
+echo
+echo aritmetic expansion:
+echo $(( $1 * $2))
+
