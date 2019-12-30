0b89b310ae0d57f7e9f971b13a40ba21f3a12d47
Matt <mjkern@andrew.cmu.edu>
2019-12-29 19:43:59 -0500

    it works! (exercise 3 - mastermind)

--- 6-loops/3-mastermind.sh
+++ 6-loops/3-mastermind.sh
@@ -7,8 +7,6 @@ for I in {1..4}
 do
   CODE="$CODE$(( $RANDOM % 4 + 1 )) "
 done
-echo the secret code is $CODE
-echo
 
 # notify user
 echo Welcome to mastermind! I already have thought of a secret code.
@@ -17,11 +15,15 @@ echo than once. The code is four numbers long. You may begin
 echo guessing by entering four \(space-separated\) numbers.
 echo
 
+# misc
+NUM_GUESSES=0
+
 ### PLAY GAME ###
 until [ $NUM_CORRECT -eq 4 ]
 do
   # get the guess
   read -p "enter a guess: " GUESS
+  (( NUM_GUESSES++ ))
 
   # count the correct characters
   NUM_CORRECT=0
@@ -62,3 +64,4 @@ done
 
 ### GAME OVER ###
 echo you did it!
+echo "it took you $NUM_GUESSES guess(es)"
