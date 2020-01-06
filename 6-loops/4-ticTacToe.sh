56a6376d3ddd665d442dec16de29516255416a8f
Matt <mjkern@andrew.cmu.edu>
2020-01-05 19:32:52 -0500

    can now print new board representation

--- 6-loops/4-ticTacToe.sh
+++ 6-loops/4-ticTacToe.sh
@@ -6,13 +6,9 @@
 echo Welcome to tic tac toe!
 
 # init state
-#BOARD='1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n'
-#printf $BOARD
-
 declare -A BOARD
 for i in {1..9}
 do
-  echo $(( ($i - 1) / 3 )), $(( ($i - 1) % 3 )): $i
   BOARD[$(( ($i - 1) / 3)),$(( ($i - 1) % 3 ))]=$i
 done
 
@@ -21,8 +17,16 @@ for i in {0..2}
 do
   for j in {0..2}
   do
-    printf ${BOARD[$i,$j]}
+    printf "${BOARD[$i,$j]}"
+    if [ ! $j -eq 2 ]
+    then
+      printf "|"
+    fi
   done
   echo
+  if [ ! $i -eq 2 ]
+  then
+    printf -- '-+-+-\n'
+  fi
 done
 
