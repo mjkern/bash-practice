5457cc766d2996eaf444faadbf5ba872e663b1aa
Matt <mjkern@andrew.cmu.edu>
2020-01-05 19:22:58 -0500

    working on alternative board representation

--- 6-loops/4-ticTacToe.sh
+++ 6-loops/4-ticTacToe.sh
@@ -6,5 +6,23 @@
 echo Welcome to tic tac toe!
 
 # init state
-BOARD='1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n'
-printf $BOARD
+#BOARD='1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n'
+#printf $BOARD
+
+declare -A BOARD
+for i in {1..9}
+do
+  echo $(( ($i - 1) / 3 )), $(( ($i - 1) % 3 )): $i
+  BOARD[$(( ($i - 1) / 3)),$(( ($i - 1) % 3 ))]=$i
+done
+
+# print the board
+for i in {0..2}
+do
+  for j in {0..2}
+  do
+    printf ${BOARD[$i,$j]}
+  done
+  echo
+done
+
